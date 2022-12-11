e971272e67553ddb5c817d603dc048b422847365
Alex <alexmwangithuku001@gmail.com>
2022-12-11 21:30:01 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+  
+let random = Math.random()
+  // console.log(random)
 
-// Write your code here
+let num3 = 19
+let num4 = 5
+let mod = 19%5
+
+let max = Math.max(20,1)
